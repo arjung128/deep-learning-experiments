04ca4435a3c52b35395ed9afbbcd6bd64e64e2e8
arjung128 <arjung2@illinois.edu>
2018-06-02 14:52:34 +0900

    trained toy model

---
+++ lstm.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
